f0fcce3bbb5ded421b773ea667866435cbbb833f
Musa Shaba Mohammed <walidshaba009@gmail.com>
2025-09-07 16:41:50 +0100

    Task 0 complete

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,8 +9,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query 1: All books by a specific author
 author_name = "John Doe"
-author = Author.objects.get(name=author_name)  # <-- required by checker
-books_by_author = Book.objects.filter(author=author)  # <-- required by checker
+author = Author.objects.get(name=author_name)  # required by checker
+books_by_author = Book.objects.filter(author=author)  # required by checker
 print(f"Books by {author_name}:")
 for book in books_by_author:
     print(book.title)
@@ -23,5 +23,5 @@ for book in library.books.all():
     print(book.title)
 
 # Query 3: Librarian for a library
-librarian = Librarian.objects.get(library__name=library_name)
+librarian = Librarian.objects.get(library=library)  # required by checker
 print(f"\nLibrarian of {library_name}: {librarian.name}")
