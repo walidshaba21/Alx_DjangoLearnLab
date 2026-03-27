66ad7524e30fab1d07505cea04ba21954ba76cf6
Musa Shaba Mohammed <walidshaba009@gmail.com>
2026-03-27 09:13:51 +0100

    Cleaning

--- Django_revamp/myproject/book_store/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookStoreConfig(AppConfig):
-    name = 'book_store'
--- Task_Redo/LibraryProject/bookshelf/views.py
+++ Task_Redo/LibraryProject/bookshelf/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
+from django.views.generic import DetailView
+from .models import Book
 # Create your views here.
